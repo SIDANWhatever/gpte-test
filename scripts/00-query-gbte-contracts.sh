9ed55d81306f564c837ba2280ffd8dba33ca157b
jamesdunseith <jdunseith@gimbalabs.io>
2022-10-10 06:55:07 -0400

    Fix script

--- scripts/00-query-gbte-contracts.sh
+++ scripts/00-query-gbte-contracts.sh
@@ -6,7 +6,7 @@ export TREASURY_ADDR=addr_test1wphcpc9rnyyrgzgfvlmzurfcd998gz8lek4d3026xtf66dqck
 export ESCROW_ADDR=addr_test1wq8lk00x7zjum9ys2tyeyd9ljf57ge8pcxyhnswkh8srdac4rsjss
 export REFERENCE_ADDRESS=addr_test1qqe5wnjzkhrgfvntj3dndzml7003h0n5ezen924qjrrglv6648u33jzvq2msza6gyqdcnau0njhav2sv46adkc9c8wdqx5aas8
 
-export CARDANO_NODE_SOCKET_PATH=/Users/gimbalabs/cardano/testnets/pre-prod/db/node.socket
+export CARDANO_NODE_SOCKET_PATH=/<YOUR PATH TO>/pre-prod/db/node.socket
 
 cardano-cli query tip --testnet-magic 1
 cardano-cli query protocol-parameters --testnet-magic 1 --out-file protocol.json
